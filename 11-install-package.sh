3601a35095748a4b818e72cf557c0ebcd8bcfdf7
tea0112 <ducthaidev@gmail.com>
2022-12-29 11:27:54 +0700

    update

--- 11-install-package.sh
+++ 11-install-package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pacman -S grub efibootmgr sudo iw wpa_supplicant dialog git lshw xorg networkmanager xorg-server xorg-apps xorg-xinit htop unzip wget pulseaudio alsa-utils alsa-plugins pulseaudio-bluetooth pulseaudio alsa-utils alsa-plugins bluez bluez-utils tlp tlp-rdw powertop acpi
+pacman -S grub efibootmgr sudo iw wpa_supplicant dialog lshw xorg networkmanager xorg-server xorg-apps xorg-xinit htop unzip wget pulseaudio alsa-utils alsa-plugins pulseaudio-bluetooth pulseaudio alsa-utils alsa-plugins bluez bluez-utils tlp tlp-rdw powertop acpi
 
 systemctl enable NetworkManager.service
 systemctl enable sshd
--- 6-install-system.sh
+++ 6-install-system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pacman -Sy archlinux-keyring && pacman -Su
-pacstrap /mnt base linux linux-firmware vim nano neovim tmux
+pacstrap /mnt base linux linux-firmware vim nano neovim tmux git
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
