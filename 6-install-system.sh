2a5f9a11a0e2c25edd1526bf1418de6b044e9f75
tea0112 <ducthaidev@gmail.com>
2022-12-29 10:30:19 +0700

    update

--- 6-install-system.sh
+++ 6-install-system.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+pacman -Sy archlinux-keyring && pacman -Su
 pacstrap /mnt base linux linux-firmware vim nano neovim tmux sudo
 genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt
--- install-arch-chroot.sh
+++ install-arch-chroot.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-echo "your hostname"
-read HOST_NAME
-# Create the hostname file
-echo "${HOST_NAME}" > /etc/hostname
-# edit /etc/hosts file
-echo "127.0.0.1        localhost" >> /etc/hosts
-echo "::1              localhost" >> /etc/hosts
-echo "127.0.1.1        ${HOST_NAME}" >> /etc/hosts
+./6-install-system.sh
