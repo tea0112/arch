9a902febf49cc8d27768f08a87979d2ce0cd9a84
tea0112 <ducthaidev@gmail.com>
2022-12-29 11:32:42 +0700

    update

--- 14-set-bluetooth.sh
+++ 14-set-bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed -i '/#AutoEnable/s/^#//g' main.conf
-sed -i '/AutoEnable/s/false/true/g' main.conf
-sed -i '/DiscoverableTimeout/s/^#//g' main.conf
+sed -i '/#AutoEnable/s/^#//g' /etc/bluetooth/main.conf
+sed -i '/AutoEnable/s/false/true/g' /etc/bluetooth/main.conf
+sed -i '/DiscoverableTimeout/s/^#//g' /etc/bluetooth/main.conf
 
 echo "load-module module-bluetooth-policy" >> /etc/pulse/system.pa
 echo "load-module module-bluetooth-discover" >> /etc/pulse/system.pa
