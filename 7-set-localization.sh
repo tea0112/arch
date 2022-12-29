6f1a4d0d6096b74d238cefa054956c3989788686
tea0112 <ducthaidev@gmail.com>
2022-12-29 11:38:38 +0700

    update

--- 7-set-localization.sh
+++ 7-set-localization.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # uncomment en_US.UTF-8 UTF-8
-sed -e '/en_US.UTF-8/s/^# *//g' -i /etc/locale.gen
-locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
+
 export LANG=en_US.UTF-8
+
+sed -e '/en_US.UTF-8/s/^# *//g' -i /etc/locale.gen
+
+locale-gen
