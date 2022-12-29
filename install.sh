6e4e88be19a87fd1ae20abef82a2d23c66c38ad3
tea0112 <ducthaidev@gmail.com>
2022-12-29 09:47:44 +0700

    update

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ echo "type device"
 read dev_sdx
 ./2-wipe-all.sh "${dev_sdx}"
 ./3-make-gpt.sh "${dev_sdx}"
-./4-make-partition "${dev_sdx}"
-./5-format-partition "${dev_sdx}"
+./4-make-partition.sh "${dev_sdx}"
+./5-format-partition.sh "${dev_sdx}"
