f081b9278dd4dfbb24a215899096c19e5c2cdfe3
tea0112 <ducthaidev@gmail.com>
2023-08-18 16:08:21 +0700

    update

--- dependencies/localtion.sh
+++ dependencies/localtion.sh
@@ -2,8 +2,7 @@
 
 ################ Set localization
 echo "*** Set localization"
-export LANG=en_US.UTF-8
-echo LANG=en_US.UTF-8 > /etc/locale.conf
 sed -e '/en_US.UTF-8/s/^# *//g' -i /etc/locale.gen
 locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
 
