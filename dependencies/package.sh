a6ac2832ccbded6891a04d0e220b7d143a19010c
tea0112 <ducthaidev@gmail.com>
2023-08-18 19:23:09 +0700

    update

--- dependencies/package.sh
+++ dependencies/package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "*** Install package and enable service"
-pacman -S grub efibootmgr sudo iw wpa_supplicant dialog lshw xorg networkmanager xorg-server xorg-apps xorg-xinit htop unzip wget alsa-utils alsa-plugins bluez bluez-utils openssh dhcpcd docker noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-liberation ttf-droid ttf-roboto terminus-font rxvt-unicode ranger rofi dmenu firefox lightdm lightdm-gtk-greeter lxappearance arc-gtk-theme papirus-icon-theme alacritty pipewire pipewire-audio pipewire-alsa wireplumber pipewire-jack i3-wm pipewire-pulse ntfs-3g
+pacman -S grub efibootmgr sudo iw wpa_supplicant dialog lshw xorg networkmanager xorg-server xorg-apps xorg-xinit htop unzip wget alsa-utils alsa-plugins bluez bluez-utils openssh dhcpcd docker noto-fonts ttf-ubuntu-font-family ttf-dejavu ttf-freefont ttf-liberation ttf-droid ttf-roboto terminus-font rxvt-unicode ranger rofi dmenu firefox lightdm lightdm-gtk-greeter lxappearance arc-gtk-theme papirus-icon-theme alacritty pipewire pipewire-audio pipewire-alsa wireplumber pipewire-jack i3-wm pipewire-pulse ntfs-3g flameshot
 
 echo "Enable Services"
 systemctl enable lightdm
