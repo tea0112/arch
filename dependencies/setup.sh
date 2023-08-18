5c110dec2c439b2b24d67df5a6eab986d41562f9
tea0112 <ducthaidev@gmail.com>
2023-08-18 10:06:52 +0700

    update

--- dependencies/setup.sh
+++ dependencies/setup.sh
@@ -3,9 +3,10 @@
 pacman -Sy archlinux-keyring && pacman -Su
 pacstrap /mnt base linux linux-firmware base-devel vim nano neovim tmux git
 genfstab -U /mnt >> /mnt/etc/fstab
+
 mkdir /mnt/install-script
-cp /root/arch /mnt/install-script
-echo "Finish installing ISO!"
-echo "Run ----> arch-chroot /mnt"
-echo "Run install-script/arch-chroot.sh"
+cp /root/arch /mnt/install-script -r
+echo "********************************* Finish installing ISO!"
+echo "********************************* Run ----> arch-chroot /mnt"
+echo "********************************* Run install-script/arch/arch-chroot.sh"
 
